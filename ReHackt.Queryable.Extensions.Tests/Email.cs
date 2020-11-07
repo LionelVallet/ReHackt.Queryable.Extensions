7ff17359bfd149f0cf4a749d2bb3265b8b33775a
Thibaut Debruille <thibaut@debruille.fr>
2020-11-07 16:13:24 +0100

    add unit tests

---
+++ ReHackt.Queryable.Extensions.Tests/Email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReHackt.Queryable.Extensions.Tests
+{
+    public class Email
+    {
+        private readonly string _email;
+        public Email(string email)
+        {
+            if (!email.Contains("@")) throw new ArgumentException("An e-mail must contain the @ character.");
+
+            _email = email;
+        }
+
+        public static implicit operator string(Email email) => email._email;
+
+        public override string ToString() => _email;
+    }
+}
